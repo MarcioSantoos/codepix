51f464cb2ae8f4dd53721e6abdf2e5edd0538f48
Márcio Santos <saturno17.mv@gmail.com>
2023-10-19 20:03:24 -0300

    feat: implemented cobra-cli and finished grpc server(some files that not pushed).

---
+++ cmd/grpc.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/codeedu/imersao/codepix-go/application/grpc"
+	"github.com/codeedu/imersao/codepix-go/infrastructure/db"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var portNumber int
+
+// grpcCmd represents the grpc command
+var grpcCmd = &cobra.Command{
+	Use:   "grpc",
+	Short: "Start a grpc server",
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.ConnectDB(os.Getenv("env"))
+		grpc.StartGrpcServer(database, portNumber)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(grpcCmd)
+	grpcCmd.Flags().IntVarP(&portNumber, "port", "p", 50501, "gRPC port")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// grpcCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// grpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
